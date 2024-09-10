13a2148aa949906a1b5456b1b953574a78490418
Ali <126307892+alicode43@users.noreply.github.com>
2024-09-10 16:33:14 +0530

    pahila

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+ export interface Video {
+    id:string
+    title:string
+    originalSize:number
+    publicId:string
+    compressedSize:number
+    createdAt:Date
+    duration:number
+    description:string
+ }
